14aa9a3268192eef954ccb7f24c40c4298bcc2ba
midore <midorex@gmail.com>
2009-12-18 14:48:38 +0900

    change IO.foreach

--- lib/mdiary/mdiary-io.rb
+++ lib/mdiary/mdiary-io.rb
@@ -22,44 +22,36 @@ module Mdiary
         break if line.match(mark)
         a.push(line) 
       }
-      return ary_to_h(a)
-    end
-
-    def search_plus(path)
-      h = view_h(path)
-      return h unless h[:control] == 'yes'
-    end
-
-    def search_h(path)
-      return find_index(path) unless @st
-      return find_content(path)
+      return ary_to_h(a, path)
     end
 
     def find_index(path)
       h = view_h(path)
+      return h unless h[:control] == 'yes' if @plus
       m = h.values.select{|v| v.match(@word)}
       return h unless m.empty?
     end
 
     def find_content(path)
+      i, m = false, false
       a, mark = [], /^--content$/
-      i, m = nil, false
       IO.foreach(path){|line|
         i = true if line.match(mark)
         m = true if line.match(@word)
         a.push(line) unless i
-        break if i and m
+        break if m
       }
-      return ary_to_h(a) if m
+      return ary_to_h(a, path) if m
     end
 
-    def ary_to_h(ary)
+    def ary_to_h(ary, path)
       h, k = Hash.new, nil
       ary.each{|x|
         hit = x.match(/--(.*)\n$/)
         k = hit[1].to_sym if hit
         h[k] = x.strip unless hit
       }
+      h[:path] = path
       return h
     end
  
