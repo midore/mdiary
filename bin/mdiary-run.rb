3609b6a4e31daeade6c174e651a02f1137e49d41
midore <midorex@gmail.com>
2010-01-04 01:18:53 +0900

    add err

--- bin/mdiary-run.rb
+++ bin/mdiary-run.rb
@@ -2,22 +2,40 @@
 
 #------------------------------------------
 # mdiary-run.rb
-# last: 2009-12-19
 # ruby 1.9.1p376 
+# 2010-01-04
 #------------------------------------------
 
+def check_arg(arg)
+  err = nil
+  # ENV['LANG']
+  ext = Encoding.default_external.name
+  return err = 'lang' unless ext == 'UTF-8'
+  # ARGV.size
+  return err = 'err0' unless arg[0]
+  arg.each_with_index{|x,no|
+    y = no%2
+    err = 'err1' if y == 0 and x.size > 6
+    err = 'err2' if y == 1 and x.size > 20
+  }
+  return err
+end
+
 dir = File.dirname(File.dirname(File.expand_path($PROGRAM_NAME)))
-$LOAD_PATH.push(File.join(dir, 'bin'), File.join(dir, 'lib'))
+bin = File.join(dir, 'bin')
+lib = File.join(dir, 'lib')
+$LOAD_PATH.push(bin, lib)
 $LOAD_PATH.delete(".")
 
-ext = Encoding.default_external.name
-err = nil
-err = true unless ext == 'UTF-8'
-err = true if ARGV.size > 4
-ARGV.each{|v| err = true if v.size > 20}
-abort if err
+arg = ARGV
+arg.delete("")
+err = check_arg(arg)
+exit unless err.nil?
 
-load 'mdconfig', wrap=true
+conf = File.join(bin, 'mdconfig')
+exit unless File.exist?(conf)
+
+load conf, wrap=true
 require 'mdiary'
-Mdiary::Main.new().start(ARGV)
+Mdiary::Main.new().start(arg)
 
