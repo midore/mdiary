fb9c33176e791789a65bd2950b6eedf4c504d54c
midore <midorex@gmail.com>
2010-06-08 16:59:57 +0900

    ruby1.9.2

--- bin/mdiary-run.rb
+++ bin/mdiary-run.rb
@@ -1,10 +1,12 @@
-#!/usr/bin/local/ruby19
+#!/path/to/ruby19
 # coding: utf-8
 
 #------------------------------------------
 # mdiary-run.rb
 # ruby 1.9.1p376 
-# 2010-02-24
+
+# 2010-06-02
+# ruby 1.9.2dev (2010-05-31 revision 28117) [x86_64-darwin10.3.0]
 #------------------------------------------
 
 exit unless Encoding.default_external.name == 'UTF-8'
