2051f5e791c35566eeba8e58e1b2ad0f9d05ce5d
midore <midorex@gmail.com>
2010-07-01 17:27:48 +0900

    change dir check

--- lib/mdiary/mdiary-io.rb
+++ lib/mdiary/mdiary-io.rb
@@ -5,12 +5,11 @@ module Mdiary
     include $MDIARYCONF
 
     def check_conf
-      dir = data_dir
-      days = text_dir
-      trash = trash_dir
-      return errmsg(dir) unless check_dir(days)
-      return errmsg(days) unless check_dir(days)
-      return errmsg(trash) unless check_dir(trash)
+      (print "Error: Not found #{scpt_path}\n"; exit) unless File.exist?(scpt_path)
+      days = File.join(data_dir, 'text')
+      trash = File.join(data_dir, 'trash')
+      errmsg(days) unless check_dir(days)
+      errmsg(trash) unless check_dir(trash)
       @text_dir, @trash_dir = days, trash
       return true
     end
@@ -26,8 +25,10 @@ module Mdiary
     end
 
     def errmsg(d=nil)
-      print "Please: $ mkdir #{d} or $ chmod +x #{d}\n" if d
-      print "Error: Directory. Need edit config file.(bin/mdconfig)\n" unless d
+      begin
+        Dir.mkdir(d)
+        print "maked directory #{d}\n"
+      end
     end
 
   end
