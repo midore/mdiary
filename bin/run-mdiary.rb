ecc0ba3162eaca0c28ba8f24ad4a0a0798a9f7f3
midore <midorex@gmail.com>
2011-11-04 23:13:43 +0900

    2011-11-04

--- bin/run-mdiary.rb
+++ bin/run-mdiary.rb
@@ -9,33 +9,21 @@
 (print "Error: LANG"; exit) unless Encoding.default_external.name == 'UTF-8'
 
 module Mdiary
-
   class Start
-
-    def own_dir
-      dir = File.dirname(File.dirname(File.expand_path($PROGRAM_NAME)))
-      lib = File.join(dir, 'lib')
-      $LOAD_PATH.push(lib)
-      $LOAD_PATH.delete(".")
+    def initialize
+      # $LOAD_PATH.delete(".")
+      conf_path = '/path/to/mdiary-conf'
+      (print "Error: not found xdiary-conf\n"; exit) unless File.exist?(conf_path)
+      load(conf_path)
     end
-
     def run
-      own_dir
-      conf = File.join(ENV['HOME'], '.m_diary', 'mdiary_conf')
-      (print "Error: not found mdiary_conf\n"; exit) unless File.exist?(conf)
-      load conf, wrap=true
       require 'mdiary'
-      # check arg
       err, arg_h = CheckStart.new(ARGV).base
       exit if err == 'help'
       (print "#{err}\n"; exit) if err
-      Main.new().start(arg_h)
+      Main.new(arg_h)
     end
-
-    private :own_dir
-
   end
-
 end
 
 Mdiary::Start.new.run
